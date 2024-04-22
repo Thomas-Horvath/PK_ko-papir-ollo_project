9f3765e045374b7cf4e27e3c4dbfeb1510a4251d
Thomas-Horvath <thomas.horvath0416@gmail.com>
2024-04-22 18:08:05 +0200

    html and style

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-const userValue = prompt('Válassz a köetkezők közül: kő, papír, olló');
-const computerValue = ["kő", "papír", "olló"][Math.floor(Math.random() * 3 )];  // számítógép ezekből választhat
+//const userValue = prompt('Válassz a köetkezők közül: kő, papír, olló');
+//const computerValue = ["kő", "papír", "olló"][Math.floor(Math.random() * 3)];  // számítógép ezekből választhat
 
-console.log(computerValue , userValue);
+// alertes megvalósítás:  
 
-//1 két érték eggyezik
-if (computerValue === userValue) {
-    alert( ` ${userValue} - ${computerValue} - Döntetlen`)
-}
+// if (computerValue === userValue) {
+//     alert(` ${userValue} - ${computerValue} - Döntetlen`)
+// } else if (userValue === "kő") {
+//     if (computerValue === "papír") {
+//         alert(` ${userValue} - ${computerValue} - Számítógép nyert`)
+//     } else /*(computerValue === "olló") */ {
+//         alert(` ${userValue} - ${computerValue} - Felhasználó nyert`)
+//     }
+// } else if (userValue === "papír") {
+//     if (computerValue === "olló") {
+//         alert(` ${userValue} - ${computerValue} - Számítógép nyert`)
+//     } else /*(computerValue === "kő") */ {
+//         alert(` ${userValue} - ${computerValue} - Felhasználó nyert`)
+//     }
+// } else if (userValue === "olló") {
+//     if (computerValue === "kő") {
+//         alert(` ${userValue} - ${computerValue} - Számítógép nyert`)
+//     } else /*(computerValue === "papír") */ {
+//         alert(` ${userValue} - ${computerValue} - Felhasználó nyert`)
+//     }
+// } else {
+//     alert('Az érték nem megfelő')
+// }
+
+
+const APP = document.querySelector('#app');
 
